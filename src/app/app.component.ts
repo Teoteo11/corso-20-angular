e9212cc05fff3a94e13c383abea6d61b79f096bc
Teoteo11 <teo.annaro@gmail.com>
2024-02-05 13:20:24 +0100

    feat(): data binding and directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-corso-20';
+
+/*   numCourse = '20';
+  disableBtn = false;
+  username = '';
+
+  triggerBtn = (event: any) => {
+    console.log("🚀 ~ AppComponent ~ $event:", event);
+    this.numCourse = '21';
+    this.disableBtn = true;
+  }
+ */
+  
+  /* In Javascript
+  <button id="test">Attiva e cambia</button>
+    document.getElementById('test').addEventListener('onclick', () => {
+      console.log('click);
+    }) 
+  */
+
+/*   isLoggedIn = false;
+
+  log = () => this.isLoggedIn = !this.isLoggedIn; */
+
+  names = ['Tizio', 'Caio', 'Sempronio'];
 }
